22917a38b447df9a960b91433ea9ebbbf2ffb8a3
lijiacen <lijiacen880915@163.com>
2020-01-19 15:15:54 +0800

    增加注释

--- src/create/factory.js
+++ src/create/factory.js
@@ -1,16 +1,17 @@
 class Product {
   constructor(name) {
-    this.name = name
+    this.name = name;
   }
   init() {}
 }
 
+//将new 操作单独封装，提供创建对象的公共接口
 class Creator {
   create(name) {
-    return new Product(name)
+    return new Product(name);
   }
 }
 
-let creator = new Creator()
-let p = creator.create("p1")
-p.init()
+let creator = new Creator();
+let p = creator.create("p1");
+p.init();
--- src/create/singleton.js
+++ src/create/singleton.js
@@ -1,48 +1,50 @@
 class Singleton {}
 
+//系统中唯一使用，并提供全局访问
 Singleton.getInstance = (function() {
-  let instance
+  //一个类只有一个实例
+  let instance;
   return function() {
     if (!instance) {
-      instance = new Singleton()
+      instance = new Singleton();
     }
-    return instance
-  }
-})()
+    return instance;
+  };
+})();
 
 class LoginForm {
   constructor() {
-    this.state = "hide"
+    this.state = "hide";
   }
   show() {
     if (this.state === "show") {
-      alert("已经显示")
-      return
+      alert("已经显示");
+      return;
     }
-    this.state = "show"
+    this.state = "show";
   }
   hide() {
     if (this.state === "hide") {
-      alert("已经隐藏")
-      return
+      alert("已经隐藏");
+      return;
     }
-    this.state = "hide"
+    this.state = "hide";
   }
 }
 LoginForm.getInstance = (function() {
-  let instance
+  let instance;
   return function() {
     if (!instance) {
-      instance = new LoginForm()
+      instance = new LoginForm();
     }
-    return instance
-  }
-})()
+    return instance;
+  };
+})();
 
-let lg1 = LoginForm.getInstance()
-lg1.show()
+let lg1 = LoginForm.getInstance();
+lg1.show();
 
-let lg2 = LoginForm.getInstance()
-lg2.show()
+let lg2 = LoginForm.getInstance();
+lg2.show();
 
-console.log(lg1 === lg2)
+console.log(lg1 === lg2);
